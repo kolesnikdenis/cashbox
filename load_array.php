b4d2bfa585af52f968bc3b0aded5938489cfcb6d
kolesnikdenis.com <i@kolesnikdenis.com>
2016-04-19 22:53:32 +0300

    123

--- load_array.php
+++ load_array.php
@@ -107,9 +107,6 @@ function create_select(name_select,id_select,onchange_select,array,selected){
   return sel;
 }
 
-
-var table=init();
-document.getElementById("addtable").appendChild(table);  // ???????? ??????? ? ??????????)
 /* magazin name, nominal */
 function add_select(magazin_id,nominal_card){
   var id_card="";
