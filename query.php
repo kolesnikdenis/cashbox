c2d386222b567d703675e6cf1676b3ad1a21bd09
kolesnikdenis.com <i@kolesnikdenis.com>
2016-04-20 01:56:15 +0300

    worked ?:)

--- query.php
+++ query.php
@@ -28,13 +28,14 @@ $idstring = $_REQUEST['idstring'];
 $id_user = $_SESSION['login_id'];
 $set_datetime = $_REQUEST['set_datetime'];
 $eierr="no";
+$global_summ_minik=0;//minik1
+        $global_summ_rodnik=0; //rodnik2
+        $global_summ_centr=0;//3
+        $global_summ_poselok=0;//4
 
 if ( $taskk == "show_cashbox" ) {
 
-        var global_summ_minik=0;//minik1
-        var global_summ_rodnik=0; //rodnik2
-        var global_summ_centr=0;//3
-        var global_summ_poselok=0;//4
+
 
 
         if($eierr=="no"){
@@ -64,15 +65,17 @@ if ( $taskk == "show_cashbox" ) {
                   else   { $type = "добавил карточек"; }
                   $add_card=$pl[3] . " * " .  $count_add   ." = " . ( $pl[3] * $count_add);
                   $sale_magazin=$pl[3] . " * " .  $count_left   ." = " . ($pl[3] * $count_left );
-                  $out .= "<td>".$pl[count_add]."</td><td>".$pl[count_left]."</td><td>".$type."</td><td bgcolor=#f4c397>".$add_card."</td><td bgcolor=#a6e3f4>".$sale_magazin."</td></tr>";
-                  if ( $pl[name] == "rodnichek") {  $global_summ_rodnik=$global_summ_rodnik-( $pl[3] * $count_add)+$add_card; }
-                  if ( $pl[name] == "minimarcet") {  $global_summ_minik=$global_summ_minik-( $pl[3] * $count_add)+$add_card; }
+                  if ( strcmp ( $pl[name], "rodnichek" ) !== 0 ) {  global $global_summ_rodnik=$global_summ_rodnik-$sale_magazin+$add_card; }
+                  if ( $pl[name] == "minimarcet") {  $global_summ_minik=$global_summ_minik-$sale_magazin+$add_card; }
                   if ( $pl[name] == "centr") {  $global_summ_rodnik=$sale_magazin+$add_card; }
                   if ( $pl[name] == "poselok") {  $global_summ_rodnik=$sale_magazin+$add_card; }
+
+
+                  $out .= "<td>".$pl[count_add]."</td><td>".$pl[count_left]."</td><td>".$type."</td><td bgcolor=#f4c397>".$add_card."</td><td bgcolor=#a6e3f4>".$sale_magazin."</td></tr>";
                 }
                 $out.="</table>";
-                $out.="родничек продал на сумму: ".$global_summ_rodnik . "<br>"."Минимаркет на сумму: ". $global_summ_minik."<br>";
-                $out.="всего прибыль за выбраный период".( $global_summ_rodnik+  $global_summ_minik)."<br><hr>";
+                $out.="родничек продал на сумму: ".$global_summ_rodnik . "<br>Минимаркет на сумму: ". $global_summ_minik."<br>";
+                $out.="всего прибыль за выбраный период".( $global_summ_rodnik +  $global_summ_minik)."<br><hr>";
 
                 $out.="<div id='$db-$idstring'> 0 \ <a onclick=\"del_record('$idstring','$db'); return false;\">del</a> </div>";
                 $_RESULT['text'] = $out;
