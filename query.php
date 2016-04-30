56da320ad1f2940fdc8a6a201a28eaa309ba5a66
animal <i@kolesnikdenis.com>
2016-04-30 03:29:07 +0300

    1

--- query.php
+++ query.php
@@ -181,14 +181,14 @@ if ( $taskk == "edit_sql" ) {
     print mysql_error();
     while ($pl=mysql_fetch_array($res)){
         if ($pl[type_calculation] =="C"){
-            $js= "function changesql(){ \r\n ".
+            $js= "$(function() {\r\n ".
                               "var sql=\"UPDATE `accounting`.`cashbox` SET `count_add` = '\"+document.getElementById(\"add_count\").value+\"' ".
                               " WHERE `cashbox`.`id` = '".$idstring. "'\" ".
                               "\r\n document.getElementById(\"show_sql_query\").innerHTML = sql;\r\n ".
                               "\r\n".
                                "}";
             $out .= "count_add: <input name=add_count id=add_count value=\"".$pl[count_add]."\" onchange=\"alert('1');  return;\"><br>".
-                "count_left: <input name=left_count id=left_count value=\"".$pl[count_left]."\" onchange=\"changesql(); return;\"><br>".
+                "count_left: <input name=left_count id=left_count value=\"".$pl[count_left]."\" onchange=\"".$js."\"><br>".
                 "<<button name=save_sql onclick=\"save_to_sql();\">save</button>";
             $savesql="UPDATE `accounting`.`cashbox` SET `count_add` = '".$pl[count_left]."',".
                  " `count_add` = '".$pl[count_add]."', WHERE `cashbox`.`id` = ".$idstring;
