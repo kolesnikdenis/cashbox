dd2060dbf8db6bc7ad5a8ca65f33d7010f3bb4d2
animal <i@kolesnikdenis.com>
2016-04-30 03:30:31 +0300

    1

--- query.php
+++ query.php
@@ -186,7 +186,7 @@ if ( $taskk == "edit_sql" ) {
                               " WHERE `cashbox`.`id` = '".$idstring. "'\" ".
                               "\r\n document.getElementById(\"show_sql_query\").innerHTML = sql;\r\n ".
                               "\r\n".
-                               "}";
+                               "});";
             $out .= "count_add: <input name=add_count id=add_count value=\"".$pl[count_add]."\" onchange=\"alert('1');  return;\"><br>".
                 "count_left: <input name=left_count id=left_count value=\"".$pl[count_left]."\" onchange=\"".$js."\"><br>".
                 "<<button name=save_sql onclick=\"save_to_sql();\">save</button>";
