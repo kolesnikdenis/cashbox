d5d6817f8044a262d70dcf88d00d2e10bafa5c41
animal <i@kolesnikdenis.com>
2016-05-26 13:04:56 +0300

    change_dasha_show_message

--- init.js
+++ init.js
@@ -33,9 +33,11 @@ Message.prototype.render = function() {
     pText.innerHTML = this.text;
     div.appendChild(pText);
     alert("3: "+this.id_div1);
-    var id_div1=_getElementById(this.id_div1);
+    var id_div1=_getElementById(id_div);
 
-    aClose.appendChild(div);
+    /*id_div1.insertBefore(div, document.body.firstChild);
+    document.body.insertBefore(div,document.body.firstChild);*/
+    id_div1.appendChild(div);
 
     div.style.backgroundColor = '#f77d71';
     div.style.fontSize = '1.5rem';
