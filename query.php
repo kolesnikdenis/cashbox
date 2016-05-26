c4180eab173d4d273c1e9428f3bae0fe93daace5
animal <i@kolesnikdenis.com>
2016-05-26 12:53:12 +0300

    change_dasha_show_message

--- query.php
+++ query.php
@@ -212,7 +212,7 @@ if ( $taskk == "show_cashbox" ) {
                 foreach ($array_pay as $key1 =>&$value1 ) {
                     foreach ($array_pay[$key1] as $key => &$value) {
                         $out .="<tr><td>".$key1."</td><td>".$key."</td><td>".$value['ost']."</td><td>".$value['add']."</td><td>".$value['prodal'].
-                        "</td><td><a href=\"javascript:void(show_message('$value[descr]'))\"><span   title=\"".$value[descr]."\">описание фин опираций</span></a></td>".
+                        "</td><td><a href=\"javascript:void(show_message('".$key1.$key."','$value[descr]'))\"><span title=\"".$value[descr]."\">описание фин опираций</span><div id=".$key1.$key.">show</div></a></td>".
                         "<td><a href=# onclick=\"edit_sql(".$pl[id].");\">edit</a> \ <a href=#>del</a></td></tr>";
                         $global_summ[$key1]["prodal"] += $value['prodal'];
                     }
