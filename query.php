8ac0d2abf6604a54ca2bc6361f0a89393c5bcb65
animal <i@kolesnikdenis.com>
2016-05-26 12:30:01 +0300

    change_data_input

--- query.php
+++ query.php
@@ -212,7 +212,7 @@ if ( $taskk == "show_cashbox" ) {
                 foreach ($array_pay as $key1 =>&$value1 ) {
                     foreach ($array_pay[$key1] as $key => &$value) {
                         $out .="<tr><td>".$key1."</td><td>".$key."</td><td>".$value['ost']."</td><td>".$value['add']."</td><td>".$value['prodal'].
-                        "</td><td><a href=\"#\" title=\"".$value[descr]."\"><span title=\"more_info_card\">описание фин опираций</span></a></td>".
+                        "</td><td> <span   title=\"".$value[descr]."\">описание фин опираций</span></td>".
                         "<td><a href=# onclick=\"edit_sql(".$pl[id].");\">edit</a> \ <a href=#>del</a></td></tr>";
                         $global_summ[$key1]["prodal"] += $value['prodal'];
                     }
