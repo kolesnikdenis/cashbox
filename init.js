d86cd1c52d12c07aca0e3a4314c130500fc238f5
animal <i@kolesnikdenis.com>
2016-05-26 14:01:23 +0300

    change_dasha_show_message

--- init.js
+++ init.js
@@ -61,8 +61,8 @@ Message.prototype.destroy = function() {
         var id_div2=_getElementById(del_id);
         id_div2.innerHTML="";
     }
-    //submit_dasha.addEventListener("click", deleteMessage(this.id_div1), false );
-    submit_dasha.addEventListener("click", destroy(this.id_div1));
+    submit_dasha.addEventListener("click", deleteMessage(this.id_div1), false );
+    //submit_dasha.addEventListener("click", destroy(this.id_div1));
 };
 
 function show_message(id_div,in_message){
