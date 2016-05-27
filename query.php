13ebb8670465a69f4214cbc138167c9d7372f06a
animal <i@kolesnikdenis.com>
2016-05-27 19:13:08 +0300

    change_dasha_show_message

--- query.php
+++ query.php
@@ -227,23 +227,25 @@ if ( $taskk == "show_cashbox" ) {
 
                 $out.="</table><br><Br>";
 
-                $out.='<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                         <div class="modal-dialog">
-                           <div class="modal-content">
-                             <div class="modal-header">
-                               <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                               <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-                             </div>
-                             <div class="modal-body">
-                               ...
-                             </div>
-                             <div class="modal-footer">
-                               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                               <button type="button" class="btn btn-primary">Save changes</button>
-                             </div>
-                           </div>
-                         </div>
+                $out.='<div class="modal fade bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                  <div class="modal-content">
+                                                    <div class="modal-header">
+                                                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                                      <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                      ...
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                      <button type="button" class="btn btn-primary">Save changes</button>
+                                                    </div>
+                                                  </div>
+                                                </div>
                        </div>
+
+
                     ';
 
                 $out.="<div id='$db-$idstring'> 0 \ <a onclick=\"del_record('$idstring','$db'); return false;\">del</a> </div>";
