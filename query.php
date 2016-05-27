b8b001ce15fa5ecb885ee4157b879be876974c0b
animal <i@kolesnikdenis.com>
2016-05-27 19:36:40 +0300

    change_dasha_show_message

--- query.php
+++ query.php
@@ -233,23 +233,7 @@ if ( $taskk == "show_cashbox" ) {
 
                 $out.="</table><br><Br>";
 
-                $out.='<div class="modal fade bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                  <div class="modal-content">
-                                                    <div class="modal-header">
-                                                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                                                      <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-                                                    </div>
-                                                    <div id=text_t class="modal-body">
-                                                      ...
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                                      <button type="button" class="btn btn-primary">Save changes</button>
-                                                    </div>
-                                                  </div>
-                                                </div>
-                       </div>
+                $out.='
 
 
                     ';
