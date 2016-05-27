b2382aa747fb885840c8104e62a9220bf821079a
animal <i@kolesnikdenis.com>
2016-05-27 11:45:41 +0300

    change_dasha_show_message

--- index.php
+++ index.php
@@ -51,7 +51,6 @@ include "top.php";
 
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-<!-- script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 <link rel="stylesheet" href="http://185.43.251.89/bootstrap-3.3.6-dist/css/bootstrap-theme.css">
